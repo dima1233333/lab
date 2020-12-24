a2cc96c8d467ea4505abf5aec9500697282c6f77
dima1233333 <59147773+dima1233333@users.noreply.github.com>
2020-12-24 21:35:42 +0200

    Добавьте файлы проекта.

---
+++ lab 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус R: ");
+            double R = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите высоту h: ");
+            double h = double.Parse(Console.ReadLine());
+            Console.WriteLine("Обьем цилиндра = {0}", Math.PI * R * R * h);
+            Console.ReadLine();
+        }
+    }
+}
