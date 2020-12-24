1548b481badd11e5892550d411257393b3553cc1
dima1233333 <59147773+dima1233333@users.noreply.github.com>
2020-12-24 21:38:08 +0200

    add lab 2

---
+++ lab 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string selection = Console.ReadLine();
+            int n = new int();
+
+            n = Convert.ToInt32(selection);
+            if (n % 2 == 1 && n >= 100 && n <= 999)
+                Console.WriteLine("Данное число является нечетным трехзначным");
+        }
+    }
+}
